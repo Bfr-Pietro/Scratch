a9502c02d9b432e7ad48f2d79999365824d253a1
Bfr-Pietro <smapietro@gmail.com>
2024-09-30 23:21:36 -0300

    Add files via upload

---
+++ ScrollReveal.js
@@ -0,0 +1,58 @@
+const sr = ScrollReveal();
+
+sr.reveal('.jogos', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  easing: 'ease-in-out',
+  reset: true,
+});
+sr.reveal('#descricaoscratch', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  easing: 'ease-in-out',
+  reset: true,
+});
+sr.reveal('#gh', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  easing: 'ease-in-out',
+  reset: true,
+});
+sr.reveal('#jogossection>h1', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  easing: 'ease-in-out',
+  reset: true,
+});
+sr.reveal('#footerlogos', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  easing: 'ease-in-out',
+  reset: true,
+});
+sr.reveal('#footerlogos>hr', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  easing: 'ease-in-out',
+  reset: true,
+});
+sr.reveal('#footerlogos>p', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  easing: 'ease-in-out',
+  reset: true,
+});
+sr.reveal('#footerscrollreveal', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  easing: 'ease-in-out',
+  reset: true,
+});
